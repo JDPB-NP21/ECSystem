2f36995774f6f14558e2093c9b4fdd0a49535223
Kamil <kamildudek2@gmail.com>
2022-01-21 10:34:43 +0100

    Add GreeterService for proto file

---
+++ ECSystem.Server.Main/Controllers/GreeterService.cs
@@ -0,0 +1,16 @@
+﻿using Grpc.Core;
+
+namespace ECSystem.Server.Main.Controllers {
+    public class GreeterService : Greeter.GreeterBase {
+        private readonly ILogger<GreeterService> _logger;
+        public GreeterService(ILogger<GreeterService> logger) {
+            _logger = logger;
+        }
+
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context) {
+            return Task.FromResult(new HelloReply {
+                Message = $"Hello {request.Name} ; Host={context.Host} ; Peer={context.Peer}"
+            });
+        }
+    }
+}
