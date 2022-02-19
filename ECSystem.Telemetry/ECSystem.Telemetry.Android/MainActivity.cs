19cfb42d57d88158c78ba09db6e14b523557e8e2
Lukasz Jarema <lukjar006@utp.edu.pl>
2022-02-19 14:12:16 +0100

    add geolocation

--- ECSystem.Telemetry/ECSystem.Telemetry.Android/MainActivity.cs
+++ ECSystem.Telemetry/ECSystem.Telemetry.Android/MainActivity.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-
+using System.Threading;
+using System.Threading.Tasks;
 using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
@@ -8,6 +9,8 @@ using Android.Content;
 using ECSystem.Telemetry.Droid.Broadcasts;
 using Android.Widget;
 using ECSystem.Telemetry.Droid.Services;
+using Android;
+using Xamarin.Essentials;
 
 namespace ECSystem.Telemetry.Droid {
     [Activity(Label = "ECSystem.Telemetry", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
@@ -22,11 +25,66 @@ namespace ECSystem.Telemetry.Droid {
             //var myIntent = new Intent(ApplicationContext, typeof(MyService));
             //myIntent.AddFlags(ActivityFlags.NewTask);
             //ApplicationContext.StartForegroundService(myIntent);
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.M)
+            {
+                if (CheckSelfPermission(Manifest.Permission.AccessCoarseLocation) != Android.Content.PM.Permission.Granted)
+                {
+                    RequestPermissions(new string[] { Manifest.Permission.AccessCoarseLocation }, 87);
+                }
+            }
+            
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults) {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            if(Build.VERSION.SdkInt >= BuildVersionCodes.M)
+            {
+                if(CheckSelfPermission(Manifest.Permission.AccessCoarseLocation) != Android.Content.PM.Permission.Granted)
+                {
+                    RequestPermissions(new string[] { Manifest.Permission.AccessCoarseLocation }, 87);
+                }
+            }
+        }
+
+        CancellationTokenSource cts;
+        async Task GetCurrentLocation()
+        {
+            try
+            {
+                var request = new GeolocationRequest(GeolocationAccuracy.Medium,
+                    TimeSpan.FromSeconds(10));
+                cts = new CancellationTokenSource();
+                var location = await Geolocation.GetLocationAsync(request, cts.Token);
+
+                if (location != null)
+                {
+                    Console.WriteLine($"Latitude: {location.Latitude}, Longtitude: {location.Longitude}, " +
+                                      $"Altitude: {location.Altitude}");
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+
+            }
+            catch (FeatureNotEnabledException fneEx)
+            {
+
+            }
+            catch (PermissionException pEx)
+            {
+
+            }
+            catch (Exception ex)
+            {
+                
+            }
+        }
+        
     }
 }
